d3c65075bcc2b5ba1c693be2bc2213eeb1230928
Jonathan Ehwald <github@ehwald.info>
2021-10-14 00:11:51 +0200

    docs(example): use a test admob app id

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Button, TextInput } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 import UMP from 'react-native-google-ump';
 
 export default function App() {
-  const [adMobAppId, setAdMobAppId] = React.useState<string>();
-
   const requestConsentInfoUpdate = async () => {
     try {
-      const result = await UMP.requestConsentInfoUpdate({ adMobAppId });
+      const result = await UMP.requestConsentInfoUpdate();
       console.log('requestConsentInfoUpdate result:', result);
     } catch (error) {
       console.error('requestConsentInfoUpdate error:', error);
@@ -53,41 +51,18 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="ca-app-pub-xxxxxxxxxxxxxxxx~yyyyyyyyyy"
-        onChangeText={setAdMobAppId}
-      />
-      <View style={styles.spacer} />
       <Button
-        color="#2196F3"
         title="Request consent info update"
         onPress={requestConsentInfoUpdate}
       />
       <View style={styles.spacer} />
-      <Button
-        color="#2196F3"
-        title="Get consent info"
-        onPress={getConsentInfo}
-      />
+      <Button title="Get consent info" onPress={getConsentInfo} />
       <View style={styles.spacer} />
-      <Button
-        color="#2196F3"
-        title="Load consent form"
-        onPress={loadConsentForm}
-      />
+      <Button title="Load consent form" onPress={loadConsentForm} />
       <View style={styles.spacer} />
-      <Button
-        color="#2196F3"
-        title="Show consent form"
-        onPress={showConsentForm}
-      />
+      <Button title="Show consent form" onPress={showConsentForm} />
       <View style={styles.spacer} />
-      <Button
-        color="#2196F3"
-        title="Reset consent info"
-        onPress={resetConsentInfo}
-      />
+      <Button title="Reset consent info" onPress={resetConsentInfo} />
     </View>
   );
 }
@@ -97,11 +72,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     padding: 32,
-  },
-  input: {
-    borderColor: '#2196F3',
-    borderWidth: 1,
-    textAlign: 'center',
   },
   button: {
     backgroundColor: 'blue',

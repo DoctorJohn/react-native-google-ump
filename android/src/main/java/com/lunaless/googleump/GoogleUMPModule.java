fa0fe069e0e1e378dbd0aafe33df0118592f3c95
Jonathan Ehwald <github@ehwald.info>
2021-10-13 18:16:30 +0200

    feat: implement native android module

--- android/src/main/java/com/lunaless/googleump/GoogleUMPModule.java
+++ android/src/main/java/com/lunaless/googleump/GoogleUMPModule.java
@@ -1,34 +1,198 @@
 package com.lunaless.googleump;
 
-import androidx.annotation.NonNull;
+import android.app.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
+
+import com.google.android.ump.ConsentDebugSettings;
+import com.google.android.ump.ConsentForm;
+import com.google.android.ump.ConsentInformation;
+import com.google.android.ump.ConsentRequestParameters;
+import com.google.android.ump.FormError;
+import com.google.android.ump.UserMessagingPlatform;
 
 @ReactModule(name = GoogleUMPModule.NAME)
 public class GoogleUMPModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "GoogleUMP";
+  public static final String NAME = "GoogleUMP";
+  private final ConsentInformation consentInformation;
+  private ConsentForm consentForm;
 
-    public GoogleUMPModule(ReactApplicationContext reactContext) {
-        super(reactContext);
+  public GoogleUMPModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    consentInformation = UserMessagingPlatform.getConsentInformation(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+  ConsentRequestParameters buildRequestParameters(ReadableMap options) {
+    ConsentRequestParameters.Builder paramsBuilder = new ConsentRequestParameters.Builder();
+
+    if (options.hasKey("adMobAppId")) {
+      paramsBuilder.setAdMobAppId(options.getString("adMobAppId"));
     }
 
-    @Override
-    @NonNull
-    public String getName() {
-        return NAME;
+    if (options.hasKey("underAgeOfConsent")) {
+      paramsBuilder.setTagForUnderAgeOfConsent(options.getBoolean("underAgeOfConsent"));
     }
 
+    ConsentDebugSettings.Builder settingsBuilder = new ConsentDebugSettings.Builder(getReactApplicationContext());
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
+    if (options.hasKey("testDeviceHashedIds")) {
+      final ReadableArray testDeviceHashedIds = options.getArray("testDeviceHashedIds");
+
+      if (testDeviceHashedIds != null) {
+        for (int i = 0; i < testDeviceHashedIds.size(); i++) {
+          String id = testDeviceHashedIds.getString(i);
+          if (id != null) {
+            settingsBuilder.addTestDeviceHashedId(id);
+          }
+        }
+      }
     }
 
-    public static native int nativeMultiply(int a, int b);
+    if (options.hasKey("debugGeography")) {
+      settingsBuilder.setDebugGeography(options.getInt("debugGeography"));
+    }
+
+    if (options.hasKey("forceTesting")) {
+      settingsBuilder.setForceTesting(options.getBoolean("forceTesting"));
+    }
+
+    ConsentDebugSettings settings = settingsBuilder.build();
+    paramsBuilder.setConsentDebugSettings(settings);
+
+    return paramsBuilder.build();
+  }
+
+  @ReactMethod
+  public void requestConsentInfoUpdate(ReadableMap options, Promise promise) {
+    ConsentRequestParameters params = buildRequestParameters(options);
+
+    final Activity currentActivity = getCurrentActivity();
+    if (currentActivity == null) {
+      GoogleUMPUtils.rejectPromise(
+        promise,
+        "null-activity",
+        "Attempted to request consent info but current activity was null."
+      );
+      return;
+    }
+
+    consentInformation.requestConsentInfoUpdate(
+      currentActivity,
+      params,
+      new ConsentInformation.OnConsentInfoUpdateSuccessListener() {
+        @Override
+        public void onConsentInfoUpdateSuccess() {
+          promise.resolve(null);
+        }
+      },
+      new ConsentInformation.OnConsentInfoUpdateFailureListener() {
+        @Override
+        public void onConsentInfoUpdateFailure(@NonNull FormError formError) {
+          GoogleUMPUtils.rejectPromise(
+            promise,
+            GoogleUMPUtils.codeFromFormError(formError),
+            formError.getMessage()
+          );
+        }
+      });
+  }
+
+  @ReactMethod
+  public void getConsentInfo(Promise promise) {
+    WritableMap consentInfo = Arguments.createMap();
+    consentInfo.putInt("consentStatus", consentInformation.getConsentStatus());
+    consentInfo.putBoolean("consentFormAvailable", consentInformation.isConsentFormAvailable());
+    promise.resolve(consentInfo);
+  }
+
+  @ReactMethod
+  public void loadConsentForm(Promise promise) {
+    final Activity currentActivity = getCurrentActivity();
+    if (currentActivity == null) {
+      GoogleUMPUtils.rejectPromise(
+        promise,
+        "null-activity",
+        "Attempted to load the consent form but the current activity was null."
+      );
+      return;
+    }
+
+    currentActivity.runOnUiThread(() -> {
+      UserMessagingPlatform.loadConsentForm(
+        currentActivity,
+        new UserMessagingPlatform.OnConsentFormLoadSuccessListener() {
+          @Override
+          public void onConsentFormLoadSuccess(@Nullable ConsentForm consentForm) {
+            GoogleUMPModule.this.consentForm = consentForm;
+            promise.resolve(null);
+          }
+        },
+        new UserMessagingPlatform.OnConsentFormLoadFailureListener() {
+          @Override
+          public void onConsentFormLoadFailure(@NonNull FormError formError) {
+            GoogleUMPUtils.rejectPromise(
+              promise,
+              GoogleUMPUtils.codeFromFormError(formError),
+              formError.getMessage()
+            );
+          }
+        }
+      );
+    });
+  }
+
+  @ReactMethod
+  public void showConsentForm(Promise promise) {
+    final Activity currentActivity = getCurrentActivity();
+    if (currentActivity == null) {
+      GoogleUMPUtils.rejectPromise(
+        promise,
+        "null-activity",
+        "Attempted to show consent form but the current activity was null."
+      );
+      return;
+    }
+
+    currentActivity.runOnUiThread(() -> {
+      consentForm.show(
+        currentActivity,
+        new ConsentForm.OnConsentFormDismissedListener() {
+          @Override
+          public void onConsentFormDismissed(@Nullable FormError formError) {
+            if (formError == null) {
+              promise.resolve(null);
+            } else {
+              GoogleUMPUtils.rejectPromise(
+                promise,
+                GoogleUMPUtils.codeFromFormError(formError),
+                formError.getMessage()
+              );
+            }
+          }
+        }
+      );
+    });
+  }
+
+  @ReactMethod
+  public void resetConsentInfo() {
+    consentInformation.reset();
+  }
 }
---
+++ android/src/main/java/com/lunaless/googleump/GoogleUMPUtils.java
@@ -0,0 +1,30 @@
+package com.lunaless.googleump;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.WritableMap;
+import com.google.android.ump.FormError;
+
+public class GoogleUMPUtils {
+  public static void rejectPromise(Promise promise, String code, String message) {
+    WritableMap userInfo = Arguments.createMap();
+    userInfo.putString("code", code);
+    userInfo.putString("message", message);
+    promise.reject(code, message, userInfo);
+  }
+
+  public static String codeFromFormError(FormError formError) {
+    switch (formError.getErrorCode()) {
+      case FormError.ErrorCode.INTERNAL_ERROR:
+        return "consent-form-internal-error";
+      case FormError.ErrorCode.INTERNET_ERROR:
+        return "consent-form-internet-error";
+      case FormError.ErrorCode.INVALID_OPERATION:
+        return "consent-form-invalid-operation";
+      case FormError.ErrorCode.TIME_OUT:
+        return "consent-form-time-out";
+      default:
+        return "consent-form-unknown-error";
+    }
+  }
+}
